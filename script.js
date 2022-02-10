15cb1479595a955638c551fde55ce8e61a4bb933
MHD Bshr Al Bitar <mhdbshralbitar@ogrenci.beykoz.edu.tr>
2022-02-10 08:34:32 +0000

    recent updates from theia

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 function compute()
 {
-    var principal = document.getElementById("prinicpal").value;
-    var rate = document.getElementById("rate").value;
-    var years = document.getElementById("years").value;
-    var interest = prinicpal * years * rate /100;
-    var year = new Date().getFullYear()+parseInt(years);
-    var result = document.getElementById("result").value;
-    document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
+    var principal = parseFloat(document.getElementById("principal").value);
+    var rate = parseFloat(document.getElementById("rate").value);
+    var years = parseInt(document.getElementById("years").value);
+    var interest = principal * years * rate / 100;
+    var yearInTheFuture = new Date().getFullYear() + years;
+    //Create the Interest text
+    document.getElementById("result").innerHTML = "Interest : If you deposit <mark>" + principal + "</mark>,<br/>" +
+        "at an interest rate of <mark>" + rate + "</mark>,<br/>" +
+        "You will receive an amount of <mark>" + interest + "</mark>,<br/> " +
+        "in the year <mark>" + yearInTheFuture + "</mark>";
 }
 
 //update slider valuers 
